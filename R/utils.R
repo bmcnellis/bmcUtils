853e1f906da6c49f8a5d5d5ee7a148feba04f0a0
Brandon McNellis <brandon.mcnellis@gmail.com>
2018-07-27 15:42:43 -0700

    added a couple functions

--- R/utils.R
+++ R/utils.R
@@ -13,13 +13,20 @@
 #' `UnlistDate` will unlist a list of 'Date' objects without coercing them to
 #' numeric.
 #'
+#' `PullFilenameDates` extracts dates of the given format from the current
+#' working directory. Assumes that the format doesn't include alphabetic
+#' characters.
+#'
+#' `clear` is a shortcut function that (1) clears the environment, (2) runs GC,
+#' (3) clears the console, and (4) restarts R.
+#'
 #' @name utils
 #' @export
 #' @examples NumberOfDays(Sys.Date())
 utils <- function() {
   message('Available functions:')
   avail <- c(
-    'NumberOfDays', 'StrpDegMinSec', 'UnlistDate'
+    'NumberOfDays', 'StrpDegMinSec', 'UnlistDate', 'clear'
   )
   return(avail)
 }
@@ -59,4 +66,39 @@ UnlistDate <- function(x) {
   class(y) <- 'Date'
   x <- lapply(x, function(x) y <<- c(y, x))
   return(y)
+}
+#' @describeIn utils Generates a date vector from filenames.
+#' @export
+PullFilenameDates <- function(fmt) {
+  if (nchar(fmt) < 6) stop('Bad fmt input.')
+  j_funct <- function(x, n, fmt) {
+    for (j in 1:(length(x) - n)) {
+      sub <- as.Date(paste(x[seq.int(j, j + n - 1)], collapse = ''), fmt)
+      if (!is.na(sub)) {
+        return(as.character(sub))
+      }
+    }
+    return(NA)
+  }
+  #y <- gsub('[A-Za-z]', '', list.files())
+  y <- list.files()
+  n <- nchar(format(as.Date(Sys.Date()), format = fmt))
+  out <- vector('character', length(y))
+  for (i in 1:length(y)) {
+    x <- unlist(strsplit(y[i], ''))
+    out[i] <- j_funct(x = x, n = n, fmt = fmt)
+  }
+  return(out)
+}
+#' @describeIn utils Clears multiple aspects of the R environment.
+#' @export
+clear <- function() {
+  suppressWarnings(file.remove('.Rhistory'))
+  suppressWarnings(file.remove('.Rdata'))
+  invisible(gc())
+  cat("\014")
+  save.image()
+  on.exit(rm(list = ls()))
+  on.exit(invisible(.rs.restartR()), add = T)
+  invisible()
 }
